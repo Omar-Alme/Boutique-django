665ecc36bbe4840422e96cb21c3ce5350337c1a8
Omar-Alme <omar.almehdar17@gmail.com>
2024-01-15 22:49:51 +0100

    Add stripe element and styling

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OQSwkLqAapZR5NVhCwPlwQZ96XcqP5VL2y03oqsxGcAYfQHKinGSI1LcvG2qc5Wb4k1Yq7hBVhbNuJjGRvQBLi900BQCTwXZF',
+        'client_secret': 'test client secret',
     }
 
 
